6494726b79fbb22476891252db888f685bee94c9
mingi <secreata77@gmail.com>
2022-06-03 15:27:34 +0900

    aoc 2020 day4 풀이

--- src/aoc2018/day6_1.clj
+++ src/aoc2018/day6_1.clj
@@ -4,12 +4,18 @@
 
 (def input (slurp "resources/aoc2018/day6.txt"))
 
+;; {:id 0 :vertex [10 20]}
+(defn parse-vertex
+  "N, M 형태의 문자열을 좌표 벡터로 파스한다."
+  [s]
+  (->> (split s #", ")
+       (mapv #(Integer/parseInt %))))
+
 (defn parse [s]
   (->> (split-lines s)
-       (map-indexed (fn [index vertex]
-                      {:id index :vertex  (as-> vertex v
-                                            (split v #", ")
-                                            (mapv #(Integer/parseInt %) v))}))))
+       (map-indexed
+        (fn [index line]
+          {:id index :vertex (parse-vertex line)}))))
 
 (defn manhattan-distance
   [[x1 y1] [x2 y2]]
@@ -18,39 +24,44 @@
 
 (defn edges
   "4분면 양끝쪽에 해당하는 좌표집합"
-  [x-end y-end]
-  (concat (map (fn [y] [0 y]) (range y-end))
-          (map (fn [y] [x-end y]) (range y-end))
-          (map (fn [x] [x 0]) (range x-end))
-          (map (fn [x] [x y-end]) (range x-end))))
+  [x-start x-end y-start y-end]
+  (concat (map (fn [y] [x-start y]) (range y-start y-end))
+          (map (fn [y] [x-end y]) (range y-start y-end))
+          (map (fn [x] [x y-start]) (range x-start x-end))
+          (map (fn [x] [x y-end]) (range x-start x-end))))
 
 (defn shortest-distance
   "구조분해로 첫번째, 두번째 길이의 데이터를 조회해서 둘이 길이가 같은지 체크한다.
    두개가 다르다면 가장 길이가 짧은 것이 하나만 존재한다는 것이다."
   [area vertexes]
-
-  (let [[[id1 distance1] [_id2 distance2]] (->> vertexes
-                                                (map (fn [{:keys [id vertex]}]
-                                                       [id (manhattan-distance area vertex)]))
-                                                (sort-by second))]
-    (when-not (= distance1 distance2)
+  (let [[[id1 distance1] [_id2 distance2]]
+        (->> vertexes
+             (map (fn [{:keys [id vertex]}]
+                    [id (manhattan-distance area vertex)]))
+             (sort-by second))]
+    (when (not= distance1 distance2)
       id1)))
 
+
 (defn generate-areas
-  [x-end y-end]
-  (for [x (range x-end)
-        y (range y-end)]
+  [x-start x-end y-start y-end]
+  (for [x (range x-start x-end)
+        y (range y-start y-end)]
     [x y]))
 
 (defn solve-part1 [vertexes]
   (let [coordinates (map :vertex vertexes)
+        x-start (apply min (map first coordinates))
+        y-start (apply min (map second coordinates))
         x-end (apply max (map first coordinates))
         y-end (apply max (map second coordinates))
         infinite-ids (into #{}  ;; to
-                           (keep #(shortest-distance % vertexes))  ;; xform - transducer
-                           (edges x-end y-end))]  ;; from
+                           (keep (fn [edge]
+                                   (shortest-distance edge
+                                                      vertexes)))  ;; xform - transducer
+                           (edges x-start x-end y-start y-end))]  ;; from
 
-    (->> (generate-areas x-end y-end)
+    (->> (generate-areas x-start x-end y-start y-end)
          (keep (fn [area]
                  (shortest-distance area vertexes)))
          (remove infinite-ids)
@@ -59,38 +70,42 @@
          sort
          last)))
 
-(comment
-  (into #{}
-        (keep #(shortest-distance % '({:id 0, :vertex [81 157]}
-                                      {:id 1, :vertex [209 355]}
-                                      {:id 2, :vertex [111 78]}
-                                      {:id 3, :vertex [179 211]})))
-        [[81 157] [209 355]])
-
-  (->> input
-       parse
-       solve-part1))
-
-
-(defn safe-zone? [max-size-of-the-region area vertexes]
+(defn safe-zone? [max-distances area vertexes]
   (let [total-distances (reduce +
                                 (map #(max (manhattan-distance area (:vertex %)))
                                      vertexes))]
-    (< total-distances max-size-of-the-region)))
+    (< total-distances max-distances)))
 
 (def total-distances-less-than-10000? (partial safe-zone? 10000))
 
 (defn solve-part2 [vertexes]
   (let [coordinates (map :vertex vertexes)
+        x-start (apply min (map first coordinates))
+        y-start (apply min (map second coordinates))
         x-end (apply max (map first coordinates))
         y-end (apply max (map second coordinates))
-        areas (generate-areas x-end y-end)]
+        areas (generate-areas x-start x-end y-start y-end)]
 
     (->> areas
          (filter #(total-distances-less-than-10000? % vertexes))
          count)))
 
 (comment
+  (require '[aoc2018.day6_1] :reload-all)
+  (->> input
+       parse
+       solve-part1)
+
   (->> input
        parse
        solve-part2))
+
+
+
+(comment
+  (into #{}
+        (keep (fn [area] (shortest-distance area '({:id 0, :vertex [81 157]}
+                                                   {:id 1, :vertex [209 355]}
+                                                   {:id 2, :vertex [111 78]}
+                                                   {:id 3, :vertex [179 211]}))))
+        [[81 157] [209 355]]))
---
+++ src/aoc2020/day4.clj
@@ -0,0 +1,108 @@
+(ns aoc2020.day4
+  (:require [clojure.set :as set :refer [difference]]
+            [clojure.spec.alpha :as s]
+            [clojure.string :as str :refer [split]]))
+
+
+(s/def :passport/cid string?)
+(s/def :passport/byr #(<= 1920 (Integer/parseInt %) 2002))
+(s/def :passport/iyr #(<= 2010 (Integer/parseInt %) 2020))
+(s/def :passport/eyr #(<= 2020 (Integer/parseInt %) 2030))
+(s/def :passport/hcl #(re-matches #"#[0-9a-f]{6}" %))
+(s/def :passport/ecl #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+(s/def :passport/pid #(re-matches #"\d{9}" %))
+(s/def :passport/hgt (fn [{:keys [unit scalar]}]
+                       (case unit
+                         "cm" (<= 150 scalar 193)
+                         "in" (<= 59 scalar 76)
+                         false)))
+
+(s/def :passport/passport
+  (s/keys :req-un [:passport/byr
+                   :passport/iyr
+                   :passport/eyr
+                   :passport/hgt
+                   :passport/hcl
+                   :passport/ecl
+                   :passport/pid]
+          :opt-un [:passport/cid]))
+
+(def input (slurp "resources/aoc2020/day4.txt"))
+
+(def passport-required-fields #{"byr" "iyr" "eyr" "hgt" "ecl" "pid" "hcl"})
+
+(defn parse
+  "
+   입력: \"byr:1971\niyr:2017 hgt:160cm\neyr:2020 ecl:hzl\npid:1570962 ...\"
+   출력: (([\"byr\" \"1971\"] [\"iyr\" \"2017\"] [\"hgt\" \"160cm\"])
+        ([\"byr\" \"1971\"] [\"iyr\" \"2017\"] [\"hgt\" \"160cm\"])
+         ...)   
+   "
+  [input]
+  (->> (split input #"\n\n")
+       (map #(re-seq #"[^\s\n]+" %))
+       (map (fn [passport]
+              (map (fn [passport-content]
+                     (split passport-content #":"))
+                   passport)))))
+
+
+(defn have-required-fields?
+  "입력: ([\"cid\" \"286\"] [\"hgt\" \"166cm\"] [\"byr\" \"1977\"] [\"iyr\" \"2012\"] [\"pid\" \"541909675\"] [\"ecl\" \"oth\"] [\"eyr\" \"2020\"] [\"hcl\" \"#59eb12\"])
+   출력: true or false
+  "
+  [passport-fields]
+  (let [fields (set (map first passport-fields))]
+    (nil? (seq (difference passport-required-fields fields)))))
+
+(defn solve-part1
+  "입력: (([\"cid\" \"286\"] [\"hgt\" \"166cm\"] [\"byr\" \"1977\"] [\"iyr\" \"2012\"] [\"pid\" \"541909675\"] [\"ecl\" \"oth\"] [\"eyr\" \"2020\"] [\"hcl\" \"#59eb12\"])
+         ([\"cid\" \"286\"] [\"hgt\" \"166cm\"] [\"byr\" \"1977\"] [\"iyr\" \"2012\"] [\"pid\" \"541909675\"] [\"ecl\" \"oth\"] [\"eyr\" \"2020\"] [\"hcl\" \"#59eb12\"])
+         ...)
+   출력: 182
+   "
+  [passports]
+  (->> passports
+       (filter have-required-fields?)
+       count))
+
+(defn transform
+  "
+   입력: [\"byr\" \"2012\"] | [\"hgt\" \"10cm\"]
+   출력: {:byr \"2012\"}    | {:hgt {:unit \"cm\" :scalar \"187\"}}
+   "
+  [passports]
+  (map (fn [passport]
+         (reduce (fn [acc [key value]]
+                   (cond
+                     (and (= key "hgt")
+                          (not (nil? (re-seq #"(\d+)cm" value)))) (assoc acc
+                                                                         (keyword key)
+                                                                         {:unit "cm"
+                                                                          :scalar (Integer/parseInt (second (first (re-seq #"(\d+)cm" value))))})
+                     (and (= key "hgt")
+                          (not (nil? (re-seq #"(\d+)in" value)))) (assoc acc
+                                                                         (keyword key)
+                                                                         {:unit "in"
+                                                                          :scalar (Integer/parseInt (second (first (re-seq #"(\d+)in" value))))})
+                     :else (assoc acc (keyword key) value)))
+                 {}
+                 passport))
+       passports))
+
+(defn solve-part2 [passports]
+  (->> passports
+       (filter have-required-fields?)
+       transform
+       (filter #(s/valid? :passport/passport %))
+       count))
+
+(comment
+  (->> input
+       parse
+       solve-part1)
+
+
+  (->> input
+       parse
+       solve-part2))
