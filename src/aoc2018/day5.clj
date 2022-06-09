b82e8f5af580167abcce1c8611200479284a114f
mingi <secreata77@gmail.com>
2022-06-09 13:37:37 +0900

    aoc day 5 리팩토링
    - Input & Output 명시
    - reduce 함수 제거 및 각 부분문제 독립적으로 처리하도록 변경

--- src/aoc2018/day5.clj
+++ src/aoc2018/day5.clj
@@ -1,9 +1,11 @@
-(ns aoc2018.day5
-  (:require [clojure.string :refer [trim]]))
+(ns aoc2018.day5)
 
-(def input (map (comp keyword str) (slurp "resources/aoc2018/day5.txt")))
+(def input
+  (map (comp keyword str) (slurp "resources/aoc2018/day5.txt")))
 
-(def ascii-lowercases (map char (range 97 123)))
+(def units (->> (map char (range 97 123))
+                (map str)
+                (map keyword)))
 
 (def opposite-unit-pairs
   {:a :A
@@ -60,56 +62,80 @@
    :Z :z})
 
 (defn reactive?
+  "두개의 unit을 입력받아 서로 반응하는지에 대한 여부를 반환합니다.
+   Input: :x :X
+   Output: true
+   "
   [unit1 unit2]
   (= unit1
      (get opposite-unit-pairs unit2)))
 
-(get opposite-unit-pairs :a)
-
 (defn collect-unreacted-unit
+  "폴리머와 주어진 유닛이 반응을한다면 마지막 유닛을 제거한 폴리머를 반환하고
+   반응하지않는다면 폴리머 벡터에 유닛을 포함하여 반환합니다.
+   Input: [:t :r :x ..]
+   Output: [:x :r ..]
+   "
   [acc-polymer unit]
   (if (reactive? (last acc-polymer) unit)
     (pop acc-polymer)
     (conj acc-polymer unit)))
 
 (defn react
+  "주어진 polymer를 화학 반응시킵니다. 결과물로 반응한 유닛들이 제거된 polymer를 반환합니다.
+   Input: (:b :B :t :r :l :a ...)
+   Output: [:d :a :C :B :A :c :a :D :A]
+   "
   [polymer]
   (reduce collect-unreacted-unit [] polymer))
 
-(defn solve-part1 [polymer]
+(defn solve-part1
+  "입력으로 주어진 polymer를 반응시키고 난 후의 길이를 반환합니다.
+   Input: (:b :B :t :r :l :a ...)
+   Output: 10"
+  [polymer]
   (->> polymer
        react
        count))
 
+(defn remove-given-unit-pair-in-polymer
+  "주어진 유닛을 제거한 폴리머를 반환합니다.
+   Input: :b (:b :B :t :r ...)
+   Output: [:B :t :r ...]
+   "
+  [given-unit polymer]
+  (->> polymer
+       (remove (fn [unit]
+                 (or (= unit given-unit)
+                     (reactive? unit given-unit))))
+       (into [])))
+
+(defn generate-one-unit-removed-polymers
+  "폴리머를 입력받아 유닛리스트에서 하나의 유닛을 제거한 모든 폴리머들을 만들어냅니다.
+   Input: (:b :B :t :r ...)
+   Output: ([:t :r ...]
+            [:a :A ...])
+   "
+  [polymer]
+  (map #(remove-given-unit-pair-in-polymer % polymer) units))
+
+(defn solve-part2
+  "입력으로 주어진 폴리머에 대해서 모든 유닛을 하나씩 제거한 폴리머를 만들어 가장 짧은 폴리머를 찾아냅니다.
+   
+   Input (:b :B :t :r ...)
+   Output: 6188
+   "
+  [polymer]
+  (->> polymer
+       generate-one-unit-removed-polymers
+       (map react)
+       (map count)
+       sort
+       first))
+
 (comment
   (->> input
-       solve-part1))
+       solve-part1)
 
-;; 파트 2
-; a-z까지 모든 A/a B/b 지워보면서 길이가 가장 짧은 Polymer를 만드는 것
-; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
-
-(defn remove-given-unit-pair-in-polymer
-  [given-unit polymer]
-  (remove (fn [unit]
-            (or (= unit given-unit)
-                (= unit (get opposite-unit-pairs given-unit))))
-          polymer))
-
-(defn solve-part2 [polymer]
-  (reduce (fn [shortest-polymer-cnt unit] (->> polymer
-                                               (remove-given-unit-pair-in-polymer unit)
-                                               react
-                                               count
-                                               (min shortest-polymer-cnt)))
-          (count polymer)
-          ascii-lowercases))
-
-(comment
-  ;; 4
-  (->> (seq "dabAcCaCBAcCcaDA")
-       solve-part2)
-
-  ;; 6188
   (->> input
        solve-part2))
